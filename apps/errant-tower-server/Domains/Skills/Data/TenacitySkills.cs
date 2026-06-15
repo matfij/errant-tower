c92daa620a3c45dcc07585bd44a2f38b0094f962
matfij <mateusz.michal.fijak@gmail.com>
2026-06-15 20:03:25 +0200

    fix: review comments

--- apps/errant-tower-server/Domains/Skills/Data/TenacitySkills.cs
+++ apps/errant-tower-server/Domains/Skills/Data/TenacitySkills.cs
@@ -78,11 +78,11 @@ public static class TenacitySkills
         Properties =
         [
             [new () { Type = SkillPropertyType.PhysicalDeflect, Value = 0.01 }],
-            [new () { Type = SkillPropertyType.MagicalDeflect, Value = 0.02 }],
-            [new () { Type = SkillPropertyType.MagicalDeflect, Value = 0.03 }],
-            [new () { Type = SkillPropertyType.MagicalDeflect, Value = 0.04 }],
-            [new () { Type = SkillPropertyType.MagicalDeflect, Value = 0.05 }],
-            [new () { Type = SkillPropertyType.MagicalDeflect, Value = 0.06 }],
+            [new () { Type = SkillPropertyType.PhysicalDeflect, Value = 0.02 }],
+            [new () { Type = SkillPropertyType.PhysicalDeflect, Value = 0.03 }],
+            [new () { Type = SkillPropertyType.PhysicalDeflect, Value = 0.04 }],
+            [new () { Type = SkillPropertyType.PhysicalDeflect, Value = 0.05 }],
+            [new () { Type = SkillPropertyType.PhysicalDeflect, Value = 0.06 }],
             [new () { Type = SkillPropertyType.PhysicalDeflect, Value = 0.07 }],
             [new () { Type = SkillPropertyType.PhysicalDeflect, Value = 0.08 }],
             [new () { Type = SkillPropertyType.PhysicalDeflect, Value = 0.09 }],
